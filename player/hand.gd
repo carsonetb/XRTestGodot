cfb5e2e2963ce770d6bdf9f9dd4f18911f4048fc
Carson Bates <blackhole243243@gmail.com>
2025-09-07 21:19:21 -0700

    New hands and lerped pickup positions

--- player/hand.gd
+++ player/hand.gd
@@ -11,6 +11,7 @@ var holding_offset: Vector3
 var grabbables_in_area: Array[GrabbableArea]
 var extra_rotation_force: float
 var copied_collisions: Array[CollisionShape3D]
+var is_animating_pickup: bool = false
 
 func _process(_delta: float) -> void:
 	var aim_pos = get_input("aim_pos")
@@ -18,7 +19,7 @@ func _process(_delta: float) -> void:
 		position = aim_pos
 	
 	var grab_force = get_input("grab_force")
-	if grab_force && grab_force > 0.8 && !grabbables_in_area.is_empty() && !holding_object:
+	if grab_force && grab_force > 0.3 && !grabbables_in_area.is_empty() && !holding_object:
 		holding_object = grabbables_in_area[0]
 		holding_object.object.primary_hand = self
 		
@@ -34,6 +35,8 @@ func _process(_delta: float) -> void:
 		extra_rotation_force = holding_object.object.mass / hand_object.mass
 		hand_object.mass += holding_object.object.mass
 		
+		is_animating_pickup = true
+		
 		for collision: CollisionShape3D in holding_object.object.collisions:
 			var offset: Vector3 = (collision.position - grab_point.position) * holding_object.object.scale
 			var copied: CollisionShape3D = collision.duplicate()
@@ -43,7 +46,7 @@ func _process(_delta: float) -> void:
 			copied.position = hand_grab_point.position + offset
 			copied_collisions.append(copied)
 	
-	if grab_force && grab_force < 0.8 && holding_object:
+	if grab_force && grab_force < 0.5 && holding_object:
 		for collision: CollisionShape3D in copied_collisions:
 			collision.queue_free()
 		copied_collisions.clear()
@@ -51,14 +54,21 @@ func _process(_delta: float) -> void:
 			collision.disabled = false
 		holding_object.object.primary_hand = null
 		holding_object.object.linear_velocity = hand_object.linear_velocity * 1.5
+		holding_object.object.angular_velocity = hand_object.angular_velocity * 1.5
 		hand_object.mass -= holding_object.object.mass
 		holding_object = null
 		extra_rotation_force = 0
 	
 	if !holding_object:
 		return
-	holding_object.object.global_position = hand_object.local_object_point.global_position
-	holding_object.object.rotation = hand_object.rotation
+	if !is_animating_pickup:
+		holding_object.object.global_position = hand_object.local_object_point.global_position
+		holding_object.object.rotation = hand_object.rotation
+	else:
+		holding_object.object.rotation += (hand_object.rotation - holding_object.object.rotation) * 0.1
+		holding_object.object.global_position += (hand_object.local_object_point.global_position - holding_object.object.global_position) * 0.2
+		if holding_object.object.global_position.distance_to(hand_object.local_object_point.global_position) < 0.005:
+			is_animating_pickup = false
 
 func _physics_process(_delta: float) -> void:
 	var direction: Vector3 = hand_object.global_position.direction_to(global_position)
--- player/hand_object.gd
+++ player/hand_object.gd
@@ -1,5 +1,12 @@
 class_name HandObject
 extends RigidBody3D
 
+@export var is_left: bool
 @export var attach_point: Marker3D
 @export var local_object_point: Marker3D
+
+func _ready() -> void:
+	if is_left:
+		$right_hand.visible = false
+	else:
+		$left_hand.visible = false
